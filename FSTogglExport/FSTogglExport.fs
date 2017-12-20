39db8e699e602ca4543205bb746fdf0773c6622d
Matti Petrelius <matti.petrelius@gmail.com>
2017-12-20 08:38:59 +0200

    Add writing time output entries

--- FSTogglExport/FSTogglExport.fs
+++ FSTogglExport/FSTogglExport.fs
@@ -5,9 +5,12 @@ open Fetch
 open FSharp.Data
 open JsonExtensions
 open System
+open System.IO
+open System.Text.RegularExpressions
 
 type TimeEntry = { description: string; wid: int; pid: int; start: DateTime; stop: DateTime; duration: int }
 type Project = { id: int; wid: int; name: string }
+type OutputEntry = { date: DateTime; code: string; duration: double; description: string; identifier: string }
 
 let dateTimeToIso (value: DateTime) =
     value.ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffffffzzz")
@@ -16,6 +19,7 @@ let toDateRangeQuery startDate endDate = [
     "start_date", dateTimeToIso startDate; 
     "end_date", dateTimeToIso endDate
 ]
+
 let fetchTimeEntries fetchToggl startDate endDate = async {
     let query = [
         "start_date", dateTimeToIso startDate; 
@@ -85,6 +89,34 @@ let getProjects fetchToggl workspaceIds =
     |> List.map parseList
     |> List.collect toProjects
 
+let splitToIdentifierAndDescription (description:string) = 
+    if Seq.isEmpty description then
+        "", description
+    else
+        let parts = description.Split([|' '|], 2)
+        if Regex.IsMatch(parts.[0], @"([A-Z]+-\d+|\d+)") && Array.length parts > 1 
+            then parts.[0], parts.[1]
+            else "", description
+
+let mapToOutputEntries (timeEntries: TimeEntry list) projects =
+    let getProjectName pid = projects |> List.find (fun p -> p.id = pid) |> fun p -> p.name
+    let roundToHalfHours durInSecs = max (round (durInSecs / 3600.0 * 2.0) / 2.0) 0.5
+
+    // TODO: Day grouping should be done in local time
+    timeEntries
+    |> List.groupBy (fun e -> e.start.Date, e.description, e.pid)
+    |> List.sortBy (fun ((date, _, _), entries) -> date, entries |> List.map (fun e -> e.stop) |> List.max)
+    |> List.map (fun ((date, description, pid), entries) -> 
+        let identifier, description = splitToIdentifierAndDescription description
+        {
+            date = date;
+            code = getProjectName pid;
+            description = description;
+            duration = entries |> List.sumBy (fun e -> e.duration) |> float |> roundToHalfHours;
+            identifier = identifier;
+        }
+    )
+
 let run = async {
     let apiAuth = getApiAuth
     let fetchToggl = (getFetchToggl apiAuth) []
@@ -92,13 +124,25 @@ let run = async {
     let now = DateTime.Now
     let start = DateTime(now.Year, now.Month, 1)
     let stop = DateTime(now.Year, now.Month, DateTime.DaysInMonth(now.Year, now.Month))
+    
+    printfn "Fetching data..."
     let timeEntries = getTimeEntries fetchToggl start stop
-
     let workspaceIds = timeEntries |> List.map (fun e -> e.wid) |> List.distinct
     let projects = getProjects fetchToggl workspaceIds
+    let outputEntries = mapToOutputEntries timeEntries projects
+    
+    let entryToString entry =
+        sprintf "%s\t%s\t%s\t%s\t\t%s" 
+            (entry.date.ToString("dd.MM.yyyy"))
+            entry.code
+            (entry.duration.ToString("f1"))
+            entry.description
+            entry.identifier
 
-    printfn "%A" timeEntries
-    printfn "%A" projects
+    let lines = outputEntries |> List.map entryToString
+    printfn "Writing to file..."
+    File.WriteAllLines("output.txt", lines);
+    printfn "All done!"
 }
 
 [<EntryPoint>]
--- FSTogglExport/SecretSettings.fs
+++ FSTogglExport/SecretSettings.fs
@@ -4,6 +4,5 @@ open FSharp.Data
 open JsonExtensions
 
 let getApiAuth =
-    printfn "We're getting the api auth!"
     let secrets = JsonValue.Load("../../../../secrets.json")
     secrets?apiAuth.AsString()
